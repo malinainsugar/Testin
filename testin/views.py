5c9f634318b2516c03554792eab42387169832d8
malinainsugar <super.a03-a2013@yandex.ru>
2023-01-13 07:08:55 +0500

    Добавление данных в таблицы

--- testin/views.py
+++ testin/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from testin.models import Profession
+from testin.models import Profession, Years, salaryCities, vacanciesCities
 
 def index_page(reqest):
     data = {
@@ -9,17 +9,27 @@ def index_page(reqest):
 
 def demand_page(reqest):
     data = {
-        'profession': Profession.objects.get(id=1)
+        'profession': Profession.objects.get(id=1),
+        'years' : [Years.objects.get(id=i) for i in range(1, 21)],
     }
     return render(reqest, 'demand.html', context=data)
 
 def geography_page(reqest):
-    data = {}
+    data = {
+        'salaryCities' : [salaryCities.objects.get(id=i) for i in range(1, 11)],
+        'vacanciesCities' : [vacanciesCities.objects.get(id=i) for i in range(1, 11)],
+    }
     return render(reqest, 'geography.html', context=data)
 
 def skills_page(reqest):
+    skills = []
+
+    for i in range(0, 10):
+        skills.append([Years.objects.get(year=year).skills.split(', ')[i] for year in range(2015, 2023)])
+
     data = {
-        'profession': Profession.objects.get(id=1)
+        'profession': Profession.objects.get(id=1),
+        'skills': skills,
     }
     return render(reqest, 'skills.html', context=data)
 
