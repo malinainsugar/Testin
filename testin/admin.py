9e1b70caf0c07cb9df5b55032d8977abcec99dd7
malinainsugar <super.a03-a2013@yandex.ru>
2023-01-13 05:30:01 +0500

    db

--- testin/admin.py
+++ testin/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from testin.models import Profession
+from testin.models import Profession, Years, salaryCities, vacanciesCities
 
 admin.site.register(Profession)
+admin.site.register(Years)
+admin.site.register(salaryCities)
+admin.site.register(vacanciesCities)
--- testin/models.py
+++ testin/models.py
@@ -10,3 +10,42 @@ class Profession(models.Model):
     class Meta:
         verbose_name = 'Профессия'
         verbose_name_plural = 'Профессии'
+
+class Years(models.Model):
+    year = models.IntegerField('Год')
+    salaryYear = models.IntegerField('Средняя зарплата')
+    numberVacancies = models.IntegerField('Количество вакансий')
+    selectedSalaryYear = models.IntegerField('Средняя зарплата для выбранной профессии')
+    selectedNumberVacancies = models.IntegerField('Количество вакансий для выбранной профессии')
+    skills = models.TextField('Навыки')
+
+    def __str__(self):
+        return str(self.year)
+
+    class Meta:
+        verbose_name = 'Год'
+        verbose_name_plural = 'Года'
+
+class salaryCities(models.Model):
+    city = models.CharField('Город', max_length=50)
+    salary = models.IntegerField('Средняя зарплата')
+
+    def __str__(self):
+        return self.city
+
+    class Meta:
+        verbose_name = 'Город'
+        verbose_name_plural = 'Города по зарплатам'
+
+
+class vacanciesCities(models.Model):
+    city = models.CharField('Город', max_length=50)
+    vacancies = models.FloatField('Доля вакансий')
+
+    def __str__(self):
+        return self.city
+
+    class Meta:
+        verbose_name = 'Город'
+        verbose_name_plural = 'Города по кол-ву вакансий'
+    
