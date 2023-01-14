83a1ea69f2e37d839620f16299f210bdb5dbd471
malinainsugar <super.a03-a2013@yandex.ru>
2023-01-14 11:10:22 +0500

    Добавление иконки

--- django_testin/settings.py
+++ django_testin/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=1nyggom0k6yxel$mm2wvu0s0g0&q10qzb7ndeiyixm(8m@^sk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['malinainsugar.pythonanywhere.com']
 
 
 # Application definition
@@ -118,7 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/home/malinainsugar/Testin/static'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static/"),
